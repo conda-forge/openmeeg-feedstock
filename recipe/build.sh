e3053ddc88d728389c45f9594a0e0435c064e203
Isuru Fernando <isuruf@gmail.com>
2019-10-10 11:41:23 -0500

    Use generic blas by fooling cmake here.

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,7 +9,8 @@ cmake $SRC_DIR                          \
       -DCMAKE_BUILD_TYPE:STRING=RELEASE \
       -DBUILD_DOCUMENTATION:BOOL=OFF    \
       -DCMAKE_INSTALL_PREFIX=$PREFIX    \
-      -DCMAKE_PREFIX_PATH=$PREFIX    \
+      -DCMAKE_PREFIX_PATH=$PREFIX       \
+      -DOpenBLAS_LIB=$PREFIX/lib/libcblas${SHLIB_EXT} \
       -DCMAKE_INSTALL_LIBDIR=lib
 
 cmake --build . --target install --config RELEASE
