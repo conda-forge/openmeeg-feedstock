4236841fc525a85d4a81c7afc089c62b6118115a
Isuru Fernando <isuruf@gmail.com>
2019-10-10 11:26:25 -0500

    Fix build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,11 +5,11 @@ mkdir -p $BUILD_DIR && cd $BUILD_DIR
 
 cmake $SRC_DIR                          \
       -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON  \
-      -DBLA_VENDOR:STRING=OpenBLAS      \
       -DENABLE_PYTHON:BOOL=ON           \
       -DCMAKE_BUILD_TYPE:STRING=RELEASE \
       -DBUILD_DOCUMENTATION:BOOL=OFF    \
       -DCMAKE_INSTALL_PREFIX=$PREFIX    \
+      -DCMAKE_PREFIX_PATH=$PREFIX    \
       -DCMAKE_INSTALL_LIBDIR=lib
 
 cmake --build . --target install --config RELEASE
