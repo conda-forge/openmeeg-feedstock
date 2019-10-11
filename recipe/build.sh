501681b4f55c2d565d53884d2ab30cf43bb58ff2
Joan Massich <sik@visor.udg.edu>
2019-10-11 15:12:32 +0200

    fix linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,7 @@ mkdir -p $BUILD_DIR && cd $BUILD_DIR
 cmake $SRC_DIR                          \
       -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON  \
       -DENABLE_PYTHON:BOOL=ON           \
-      -DPYTHON_VERSION=2                \
+      -DPYTHON_VERSION=${PY_VER:0:1}    \
       -DCMAKE_BUILD_TYPE:STRING=RELEASE \
       -DBUILD_DOCUMENTATION:BOOL=OFF    \
       -DCMAKE_INSTALL_PREFIX=$PREFIX    \
