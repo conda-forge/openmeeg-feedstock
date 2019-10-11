c1093c8ea06249264df90a161f7d51b1fd778ec9
Joan Massich <sik@visor.udg.edu>
2019-10-11 14:45:21 +0200

    wip: trying if py2 could work out of the box

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,7 @@ mkdir -p $BUILD_DIR && cd $BUILD_DIR
 cmake $SRC_DIR                          \
       -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON  \
       -DENABLE_PYTHON:BOOL=ON           \
+      -DPYTHON_VERSION=2                \
       -DCMAKE_BUILD_TYPE:STRING=RELEASE \
       -DBUILD_DOCUMENTATION:BOOL=OFF    \
       -DCMAKE_INSTALL_PREFIX=$PREFIX    \
