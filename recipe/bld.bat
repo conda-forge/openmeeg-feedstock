4f2823da690bfd233b81bbe439d7897b6033decd
Joan Massich <sik@visor.udg.edu>
2019-10-31 17:25:09 +0100

    blind try

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,18 +4,23 @@ mkdir build_%CMAKE_CONFIG%
 pushd build_%CMAKE_CONFIG%
 
 
+echo prefix = %PREFIX%
 echo library_dirs = %LIBRARY_LIB%
 echo include_dirs = %LIBRARY_INC%
+echo library_prefix = %LIBRARY_PREFIX%
+
+dir %LIBRARY_PREFIX%
+dir %LIBRARY_PREFIX%\lib\
 
 cmake -G "NMake Makefiles"                           ^
       -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"         ^
       -DCMAKE_BUILD_TYPE:STRING=%CMAKE_CONFIG%       ^
-      -DBLA_VENDOR:STRING=OpenBLAS                   ^
       -DENABLE_PYTHON:BOOL=ON                        ^
       -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
       -DBUILD_DOCUMENTATION:BOOL=OFF                 ^
       -DVCOMP_WORKAROUND=OFF                         ^
       -DENABLE_PACKAGING:BOOL=OFF                    ^
+      -DOpenBLAS_LIB="%LIBRARY_PREFIX%\lib\" ^
       "%SRC_DIR%"
 if errorlevel 1 exit rem 1
 
