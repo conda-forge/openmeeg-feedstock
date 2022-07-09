842b56cb4687edb2cd3c911e16561c5c7251e9ae
Eric Larson <larson.eric.d@gmail.com>
2022-07-09 17:03:52 -0400

    FIX: Path

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,25 +8,25 @@ pushd build_%CMAKE_CONFIG%
 FOR /F "tokens=* USEBACKQ" %%F IN (`%PYTHON% -c "import sysconfig;print(sysconfig.get_config_var('EXT_SUFFIX'))"`) DO (
 SET EXT_SUFFIX=%%F
 )
-dir %CONDA_PREFIX%\Library\include
-dir %CONDA_PREFIX%\Library\include\openblas
+dir %LIBRARY_INC%
+dir %LIBRARY_INC%\openblas
 
-cmake -G Ninja                                                      ^
-      -DCMAKE_BUILD_TYPE:STRING=%CMAKE_CONFIG%                      ^
-      -DCMAKE_CXX_FLAGS="-I%CONDA_PREFIX%\Library\include\openblas" ^
-      -DBLA_VENDOR:STRING=OpenBLAS                                  ^
-      -DENABLE_PYTHON:BOOL=ON                                       ^
-      -DPython3_EXECUTABLE=%PYTHON%                                 ^
-      -DPython3_EXT_SUFFIX=%EXT_SUFFIX%                             ^
-      -DPYTHON_FORCE_EXT_SUFFIX:BOOL=ON                             ^
-      -DPYTHON_INSTALL_RELATIVE:BOOL=OFF                            ^
-      -DCMAKE_SYSTEM_VERSION:STRING=7                               ^
-      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%"                ^
-      -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%"                   ^
-      -DBUILD_DOCUMENTATION:BOOL=OFF                                ^
-      -DENABLE_PACKAGING:BOOL=OFF                                   ^
-      -DBUILD_SHARED_LIBS:BOOL=ON                                   ^
-      -DBLA_STATIC=ON                                               ^
+cmake -G Ninja                                        ^
+      -DCMAKE_BUILD_TYPE:STRING=%CMAKE_CONFIG%        ^
+      -DCMAKE_CXX_FLAGS=-I%LIBRARY_INC%\openblas      ^
+      -DBLA_VENDOR:STRING=OpenBLAS                    ^
+      -DENABLE_PYTHON:BOOL=ON                         ^
+      -DPython3_EXECUTABLE=%PYTHON%                   ^
+      -DPython3_EXT_SUFFIX=%EXT_SUFFIX%               ^
+      -DPYTHON_FORCE_EXT_SUFFIX:BOOL=ON               ^
+      -DPYTHON_INSTALL_RELATIVE:BOOL=OFF              ^
+      -DCMAKE_SYSTEM_VERSION:STRING=7                 ^
+      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%"  ^
+      -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%"     ^
+      -DBUILD_DOCUMENTATION:BOOL=OFF                  ^
+      -DENABLE_PACKAGING:BOOL=OFF                     ^
+      -DBUILD_SHARED_LIBS:BOOL=ON                     ^
+      -DBLA_STATIC=ON                                 ^
       "%SRC_DIR%"
 if errorlevel 1 exit rem 1
 
