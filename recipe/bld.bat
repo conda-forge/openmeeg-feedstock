71d3327c4de0b642638978be5be7b1e4589e271f
Eric Larson <larson.eric.d@gmail.com>
2022-07-09 16:51:10 -0400

    FIX: Where is cblas.h

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,6 +8,8 @@ pushd build_%CMAKE_CONFIG%
 FOR /F "tokens=* USEBACKQ" %%F IN (`%PYTHON% -c "import sysconfig;print(sysconfig.get_config_var('EXT_SUFFIX'))"`) DO (
 SET EXT_SUFFIX=%%F
 )
+dir %CONDA_PREFIX%\Library\include
+dir %CONDA_PREFIX%\Library\include\openblas
 
 cmake -G Ninja                                                      ^
       -DCMAKE_BUILD_TYPE:STRING=%CMAKE_CONFIG%                      ^
@@ -18,6 +20,7 @@ cmake -G Ninja                                                      ^
       -DPython3_EXT_SUFFIX=%EXT_SUFFIX%                             ^
       -DPYTHON_FORCE_EXT_SUFFIX:BOOL=ON                             ^
       -DPYTHON_INSTALL_RELATIVE:BOOL=OFF                            ^
+      -DCMAKE_SYSTEM_VERSION:STRING=7                               ^
       -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%"                ^
       -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%"                   ^
       -DBUILD_DOCUMENTATION:BOOL=OFF                                ^
