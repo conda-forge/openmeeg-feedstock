a7a66abcf805128d0e730e36511d2cdeaf63703c
Eric Larson <larson.eric.d@gmail.com>
2022-07-09 16:41:42 -0400

    FIX: Push

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -9,21 +9,21 @@ FOR /F "tokens=* USEBACKQ" %%F IN (`%PYTHON% -c "import sysconfig;print(sysconfi
 SET EXT_SUFFIX=%%F
 )
 
-cmake -G Ninja                                             ^
-      -DCMAKE_BUILD_TYPE:STRING=%CMAKE_CONFIG%             ^
-      -DCMAKE_CXX_FLAGS="-I%CONDA_PREFIX%/Library/include" ^
-      -DBLA_VENDOR:STRING=OpenBLAS                         ^
-      -DENABLE_PYTHON:BOOL=ON                              ^
-      -DPython3_EXECUTABLE=%PYTHON%                        ^
-      -DPython3_EXT_SUFFIX=%EXT_SUFFIX%                    ^
-      -DPYTHON_FORCE_EXT_SUFFIX:BOOL=ON                    ^
-      -DPYTHON_INSTALL_RELATIVE:BOOL=OFF                   ^
-      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%"       ^
-      -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%"          ^
-      -DBUILD_DOCUMENTATION:BOOL=OFF                       ^
-      -DENABLE_PACKAGING:BOOL=OFF                          ^
-      -DBUILD_SHARED_LIBS:BOOL=ON                          ^
-      -DBLA_STATIC=ON                                      ^
+cmake -G Ninja                                                      ^
+      -DCMAKE_BUILD_TYPE:STRING=%CMAKE_CONFIG%                      ^
+      -DCMAKE_CXX_FLAGS="-I%CONDA_PREFIX%\Library\include\openblas" ^
+      -DBLA_VENDOR:STRING=OpenBLAS                                  ^
+      -DENABLE_PYTHON:BOOL=ON                                       ^
+      -DPython3_EXECUTABLE=%PYTHON%                                 ^
+      -DPython3_EXT_SUFFIX=%EXT_SUFFIX%                             ^
+      -DPYTHON_FORCE_EXT_SUFFIX:BOOL=ON                             ^
+      -DPYTHON_INSTALL_RELATIVE:BOOL=OFF                            ^
+      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%"                ^
+      -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%"                   ^
+      -DBUILD_DOCUMENTATION:BOOL=OFF                                ^
+      -DENABLE_PACKAGING:BOOL=OFF                                   ^
+      -DBUILD_SHARED_LIBS:BOOL=ON                                   ^
+      -DBLA_STATIC=ON                                               ^
       "%SRC_DIR%"
 if errorlevel 1 exit rem 1
 
