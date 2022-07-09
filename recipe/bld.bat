937cc49e9ec2521ced0a3438b043f3bdbb45e33b
Eric Larson <larson.eric.d@gmail.com>
2022-07-09 12:06:44 -0400

    FIX: Better

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,11 +5,18 @@ set CMAKE_CONFIG=Release
 mkdir build_%CMAKE_CONFIG%
 pushd build_%CMAKE_CONFIG%
 
+FOR /F "tokens=* USEBACKQ" %%F IN (`%PYTHON% -c "import sysconfig;print(sysconfig.get_config_var('EXT_SUFFIX'))"`) DO (
+SET EXT_SUFFIX=%%F
+)
+
 cmake -G Ninja                                       ^
       -DCMAKE_BUILD_TYPE:STRING=%CMAKE_CONFIG%       ^
       -DBLA_VENDOR:STRING=OpenBLAS                   ^
       -DENABLE_PYTHON:BOOL=ON                        ^
       -DPython3_EXECUTABLE=%PYTHON%                  ^
+      -DPython3_EXT_SUFFIX=%EXT_SUFFIX%              ^
+      -DPYTHON_FORCE_EXT_SUFFIX:BOOL=ON              ^
+      -DPYTHON_INSTALL_RELATIVE:BOOL=OFF             ^
       -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%"    ^
       -DBUILD_DOCUMENTATION:BOOL=OFF                 ^
