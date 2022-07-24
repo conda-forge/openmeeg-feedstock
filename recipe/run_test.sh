9d2ce61b840422e60d0983f4e023e1c5ae3683b8
Eric Larson <larson.eric.d@gmail.com>
2022-07-23 21:34:01 -0400

    FIX: Correct skip

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -ex
 ls -alR .
+export OPENMEEG_BAD_PYPY=$(python -c "import sys; int('pypy' in sys.implementation.name)")
 OPENMEEG_DATA_PATH=$PWD/data ./build_tools/cibuildwheel_test_command.sh
