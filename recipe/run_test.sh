b8ffb0e586b9c6e1e382f14529b33ab9725888c2
Eric Larson <larson.eric.d@gmail.com>
2022-07-24 09:41:31 -0400

    WIP

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 set -ex
-ls -alR .
-export OPENMEEG_BAD_PYPY=$(python -c "import sys; print(int('pypy' in sys.implementation.name))")
-OPENMEEG_DATA_PATH=$PWD/data ./build_tools/cibuildwheel_test_command.sh
+./build_tools/cibuildwheel_test_command.sh
