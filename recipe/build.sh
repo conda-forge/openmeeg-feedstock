30c1dffea9b732d73e256dcfeaddae6fd154f0d4
Eric Larson <larson.eric.d@gmail.com>
2024-05-23 09:36:48 -0400

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 BUILD_DIR=build
-mkdir -p $BUILD_DIR
-cp -a .git_archival.txt $BUILD_DIR
-cd $BUILD_DIR
+mkdir -p $BUILD_DIR/wrapping/python && cd $BUILD_DIR
+cp -av .git_archival.txt $BUILD_DIR/wrapping/python/
 
 EXT_SUFFIX=$(python -c "import sysconfig;print(sysconfig.get_config_var('EXT_SUFFIX'))")
 
