fde801dbfb8997c27163754d07d37228cf52028a
Eric Larson <larson.eric.d@gmail.com>
2026-06-10 14:18:47 -0400

    FIX: rm

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,13 +13,13 @@ echo "PYTHON=$PYTHON"
 USE_PYTHON="$PREFIX/bin/python"
 echo "USE_PYTHON=$USE_PYTHON"
 echo "Running CMAKE"
-#      -DPython3_EXT_SUFFIX=$EXT_SUFFIX  \
-#      -DPYTHON_FORCE_EXT_SUFFIX=ON      \
 cmake -GNinja                           \
       ${CMAKE_ARGS}                     \
       -DBLA_VENDOR:STRING=OpenBLAS      \
       -DENABLE_PYTHON:BOOL=ON           \
       -DPython3_EXECUTABLE="$USE_PYTHON"    \
+      -DPython3_EXT_SUFFIX=$EXT_SUFFIX  \
+      -DPYTHON_FORCE_EXT_SUFFIX=ON      \
       -DPYTHON_INSTALL_RELATIVE=OFF     \
       -DCMAKE_BUILD_TYPE:STRING=RELEASE \
       -DBUILD_DOCUMENTATION:BOOL=OFF    \
