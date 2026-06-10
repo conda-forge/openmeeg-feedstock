2e42b12b830891662cfc988e3312e71e0c63c7cb
Eric Larson <larson.eric.d@gmail.com>
2026-06-10 19:27:07 -0400

    FIX: Try

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,7 +9,6 @@ BUILD_DIR=build
 mkdir -p $BUILD_DIR && cd $BUILD_DIR
 
 # EXT_SUFFIX=$(python -c "import sysconfig;print(sysconfig.get_config_var('EXT_SUFFIX'))")
-EXT_SUFFIX=".abi3.so"
 # Linux: _openmeeg.abi3.so
 # macOS: _openmeeg.abi3.so
 # Windows: _openmeeg.pyd
@@ -25,8 +24,6 @@ cmake -GNinja \
       -DPython3_EXECUTABLE="$PYTHON" \
       -DPython3_INCLUDE_DIR:PATH=${Python_INCLUDE_DIR} \
       -DPython3_NumPy_INCLUDE_DIR=${Python_NumPy_INCLUDE_DIR} \
-      -DPython3_EXT_SUFFIX=$EXT_SUFFIX \
-      -DPYTHON_FORCE_EXT_SUFFIX=ON \
       -DPYTHON_INSTALL_RELATIVE=OFF \
       -DCMAKE_BUILD_TYPE:STRING=RELEASE \
       -DBUILD_DOCUMENTATION:BOOL=OFF \
